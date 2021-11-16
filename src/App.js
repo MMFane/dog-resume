7c3573acf9f92f5386bb37e21a1d8ee41c22d283
Sam <sam.williams@littlebytegames.com>
2021-11-15 21:08:29 -0800

    clean up and structure site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="resume">
+      <header>
+        <nav>Navigation</nav>
       </header>
+      <section className="body">
+        <article id="intro">Intro</article>
+        <article>Utility Tricks</article>
+        <article>Fun Tricks</article>
+      </section>
     </div>
   );
 }
