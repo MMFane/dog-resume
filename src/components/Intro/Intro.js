76062505405512cc7cde29e29a6dbfb4d293f945
Sam <sam.williams@littlebytegames.com>
2021-11-22 16:35:32 -0800

    style intro and photo

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,9 +5,14 @@ export default function Intro({ intro }) {
   return (
     <>
       <article id="intro">
-        <h1>Introducing Daisy Yeager</h1>
         <Headshot />
-        <p>{intro}</p>
+        <div id="intro-text">
+          <div id="spacer"></div>
+          <div>
+            <h1>Introducing Daisy Yeager</h1>
+            <p>{intro}</p>
+          </div>
+        </div>
       </article>
     </>
   );
