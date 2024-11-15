a3c3d38ac5f8a96ce890a24ff7c90c757bc6479c
Sam Aran <sam.erin.williams@gmail.com>
2024-11-14 17:31:11 -0800

    styling: remove template message

--- src/App.tsx
+++ src/App.tsx
@@ -57,13 +57,6 @@ function App() {
           <li onClick={() => deleteDog(dog.id)} key={dog.id}>{dog.name}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new dog.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
