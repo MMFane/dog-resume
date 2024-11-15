6f6c31673e2b239b3450aa5ff0c21172cbce5200
Sam Aran <sam.erin.williams@gmail.com>
2024-11-15 13:07:13 -0800

    add default dog profile image

--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -1,4 +1,5 @@
 import type { Schema } from '../../amplify/data/resource';
+import profileImg from '../assets/profile-default.png';
 
 interface DogCardProps {
   dog: Schema['Dog']['type'];
@@ -8,21 +9,29 @@ interface DogCardProps {
 function DogCard(props: DogCardProps) {
   return (
     <li
-      className="mb-2 mr-2 flex min-w-60 flex-col rounded-md border border-amber-400"
+      className="mb-4 mr-4 flex min-w-80 flex-col rounded-md border border-amber-400"
       key={props.dog.id}
     >
-      <div className="flex justify-between rounded-md bg-amber-100 p-2">
-        <h3>{props.dog.name}</h3>{' '}
-        <button
-          className="rounded px-2 py-1 text-amber-700 hover:bg-amber-300 hover:text-amber-900 active:bg-amber-500 active:text-amber-900"
-          onClick={() => props.deleteDog(props.dog.id)}
-        >
-          Delete
-        </button>
-      </div>
-      <div className="rounded-md p-2">
-        <p className="italic text-amber-700">{props.dog.description}</p>
-        <p>Weight: {props.dog.weight} lbs</p>
+      <div className="flex">
+        <img
+          className="h-32 max-w-32 rounded-l-md border-r border-amber-200 bg-amber-100"
+          src={profileImg}
+        />
+        <div className="w-full">
+          <div className="align-center flex justify-between rounded-md p-2">
+            <h3 className="p-1 font-bold">{props.dog.name}</h3>{' '}
+            <button
+              className="rounded px-2 py-1 text-amber-700 hover:bg-amber-300 hover:text-amber-900 active:bg-amber-500 active:text-amber-900"
+              onClick={() => props.deleteDog(props.dog.id)}
+            >
+              Delete
+            </button>
+          </div>
+          <div className="rounded-md p-2">
+            <p className="italic text-amber-700">{props.dog.description}</p>
+            <p>Weight: {props.dog.weight} lbs</p>
+          </div>
+        </div>
       </div>
     </li>
   );
