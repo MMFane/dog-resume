2296cdd9b29ba06c7bb9bc970cb43c2f06673176
Sam Aran <sam.erin.williams@gmail.com>
2024-11-15 09:22:48 -0800

    style whole page

--- src/App.tsx
+++ src/App.tsx
@@ -42,15 +42,23 @@ function App() {
   console.log(dogs);
 
   return (
-    <main className="container min-w-full">
-      <div className="container flex min-w-full flex-col items-center">
-        <h1 className="text-3xl font-bold">Add a Dog</h1>
+    <main className="min-h-full w-full bg-amber-50">
+      <div className="sticky top-0 flex w-full justify-end bg-amber-400 p-1">
+        <button
+          className="rounded-md bg-amber-700 p-3 text-white"
+          onClick={signOut}
+        >
+          Sign out
+        </button>
+      </div>
+      <div className="container flex min-w-full flex-col items-center p-4">
+        <h1 className="text-3xl font-bold text-amber-900">Add a Dog</h1>
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
           <h2 className="font-semibold"></h2>
           <div className="flex flex-col pt-4">
             <label htmlFor="name">Name</label>
             <input
-              className="p-1"
+              className="rounded-md border border-amber-600 p-1"
               id="name"
               placeholder="Fido"
               {...register('name', { required: true })}
@@ -60,7 +68,7 @@ function App() {
           <div className="flex flex-col pt-4">
             <label htmlFor="description">Description</label>
             <input
-              className="p-1"
+              className="rounded-md border border-amber-600 p-1"
               id="description"
               placeholder="A bubbly, bouncy boy"
               {...register('description')}
@@ -69,7 +77,7 @@ function App() {
           <div className="flex flex-col pt-4">
             <label htmlFor="description">Weight (lbs)</label>
             <input
-              className="p-1"
+              className="rounded-md border border-amber-600 p-1"
               id="weight"
               placeholder="25"
               {...register('weight', { required: true })}
@@ -81,7 +89,10 @@ function App() {
               </span>
             )}
           </div>
-          <input type="submit" />
+          <input
+            type="submit"
+            className="mt-4 rounded-md bg-amber-700 p-3 text-white"
+          />
         </form>
         <ul>
           {dogs.map(dog => (
@@ -91,7 +102,6 @@ function App() {
           ))}
         </ul>
       </div>
-      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
