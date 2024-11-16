7cc891e669549dfb720211b733c50aab480a160a
Sam Aran <sam.erin.williams@gmail.com>
2024-11-15 21:45:14 -0800

    add dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import NavLink from './components/NavLink';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-regular-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+
+type ThemeMode = 'dark' | 'light';
 
 function App() {
   const { signOut } = useAuthenticator();
+
+  const [themeMode, setThemeMode] = useState<ThemeMode>(
+    window.localStorage.getItem('themeMode')
+      ? (JSON.parse(localStorage.getItem('themeMode') as string) as ThemeMode)
+      : 'light'
+  );
+
+  useEffect(() => {
+    if (themeMode === 'dark') {
+      document.body.classList.add('dark');
+    }
+  }, []);
+
+  function toggleThemeMode() {
+    if (themeMode === 'dark') {
+      setThemeMode('light');
+      document.body.classList.remove('dark');
+      localStorage.setItem('themeMode', JSON.stringify('light'));
+    } else {
+      setThemeMode('dark');
+      document.body.classList.add('dark');
+      localStorage.setItem('themeMode', JSON.stringify('dark'));
+    }
+  }
 
   return (
     <main className="min-h-full w-full bg-amber-50 dark:bg-slate-800">
@@ -12,12 +44,28 @@ function App() {
           <NavLink linkText="Add Dog" route="/add-dog" />
           <NavLink linkText="Your Dogs" route="/dogs" />
         </div>
-        <button
-          className="rounded-md bg-amber-700 p-3 text-white hover:bg-amber-800 active:bg-amber-900 dark:border dark:border-black dark:bg-slate-800 dark:text-amber-100 dark:hover:bg-slate-950 dark:active:bg-black dark:active:text-amber-200"
-          onClick={signOut}
-        >
-          Sign out
-        </button>
+        <div className="flex items-center">
+          <ToggleButtonGroup
+            className="mr-4"
+            value={themeMode}
+            exclusive
+            onChange={toggleThemeMode}
+            aria-label="theme mode"
+          >
+            <ToggleButton value="light" aria-label="light mode">
+              <FontAwesomeIcon icon={faSun} />
+            </ToggleButton>
+            <ToggleButton value="dark" aria-label="dark mode">
+              <FontAwesomeIcon icon={faMoon} />
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <button
+            className="rounded-md bg-amber-700 p-3 text-white hover:bg-amber-800 active:bg-amber-900 dark:border dark:border-black dark:bg-slate-800 dark:text-amber-100 dark:hover:bg-slate-950 dark:active:bg-black dark:active:text-amber-200"
+            onClick={signOut}
+          >
+            Sign out
+          </button>
+        </div>
       </div>
       <div className="p-8">
         <Outlet />
