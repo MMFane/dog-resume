b961c91611e93da4a28db4cd28eedff5543c1067
Sam Aran <sam.erin.williams@gmail.com>
2024-12-01 17:10:54 -0800

    add dog detail page

--- src/components/DogDetail.tsx
+++ src/components/DogDetail.tsx
@@ -1,8 +1,48 @@
+import { useEffect, useState } from 'react';
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from '../../amplify/data/resource';
+import type { Dog } from '../types/types';
+import { useParams } from 'react-router-dom';
+import { calculateAge } from '../utils/age-utils';
+
 function DogDetail() {
-  // need to get dog data
+  const client = generateClient<Schema>();
+  const { dogId } = useParams();
+
+  const [dog, setDog] = useState<Dog>();
+  const [dogAge, setDogAge] = useState<number>(99);
+
+  const getDog = async () => {
+    if (!dogId) {
+      console.log('error!');
+    }
+    const { data: dog, errors } = await client.models.Dog.get({
+      id: dogId as string,
+    });
+    if (dog) {
+      setDog(dog);
+      setDogAge(calculateAge(dog?.birthdate as string));
+    } else {
+      console.error(errors);
+    }
+  };
+
+  useEffect(() => {
+    getDog();
+  }, []);
+
   return (
     <div>
-      <h1>Name Here</h1>
+      {!dog && <div>Details for dog not found</div>}
+      {dog && (
+        <h1>
+          Detail for dog {dogId} {dog.name}
+          <p>{dog.description}</p>
+          <p>
+            {dogAge}-year-old {dog.weight} lb {dog.breed}
+          </p>
+        </h1>
+      )}
     </div>
   );
 }
