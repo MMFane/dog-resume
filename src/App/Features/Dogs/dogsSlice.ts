01fbcd0a542042232af1794ac996bc68d7284de5
Sam Aran <sam.erin.williams@gmail.com>
2024-12-10 22:22:52 -0800

    add list dogs func to slice

--- src/App/Features/Dogs/dogsSlice.ts
+++ src/App/Features/Dogs/dogsSlice.ts
@@ -1,19 +1,69 @@
 import type { Dog } from '../../../types/types';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { RootState } from '../../store';
+import type { Schema } from '../../../../amplify/data/resource';
 
-const initialState: Dog[] = [];
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAppAsyncThunk } from '../../hooks';
+import type { RootState } from '../../store';
+import { generateClient } from 'aws-amplify/data';
+
+const client = generateClient<Schema>();
+
+interface DogsState {
+  dogs: Dog[];
+  status: 'idle' | 'pending' | 'succeeded' | 'failed';
+  error: string | null;
+}
+
+export const fetchDogs = createAppAsyncThunk(
+  'dogs/fetchDogs',
+  async () => {
+    const response = await client.models.Dog.list();
+    console.log('fetching dogs', response);
+    return response.data;
+  },
+  {
+    condition(_arg, thunkApi) {
+      const dogsStatus = selectDogsStatus(thunkApi.getState());
+      if (dogsStatus !== 'idle') {
+        // cancel if a thunk is already in progress
+        return false;
+      }
+    },
+  }
+);
+
+const initialState: DogsState = {
+  dogs: [],
+  status: 'idle',
+  error: null,
+};
 
 const dogsSlice = createSlice({
   name: 'dogs',
   initialState,
   reducers: {
     dogAdded(state, action: PayloadAction<Dog>) {
-      state.push(action.payload);
+      state.dogs.push(action.payload);
     },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchDogs.pending, state => {
+        state.status = 'pending';
+      })
+      .addCase(fetchDogs.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        // Add any fetched posts to the array
+        state.dogs.push(...action.payload);
+      })
+      .addCase(fetchDogs.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? 'Unknown Error';
+      });
   },
 });
 
 export default dogsSlice.reducer;
 export const { dogAdded } = dogsSlice.actions;
-export const selectAllDogs = (state: RootState) => state.dogs;
+export const selectAllDogs = (state: RootState) => state.dogs.dogs;
+export const selectDogsStatus = (state: RootState) => state.dogs.status;
